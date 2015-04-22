640baaa8e7f4e410997fb6443d1f6b1a1fdbafa4
Wofisdk <cody.constine@colorado.edu>
2015-04-22 09:51:01 -0600

    Added the addNode class for adding nodes to a tree

--- Final_Project/tree.cpp
+++ Final_Project/tree.cpp
@@ -3,9 +3,57 @@
 
 tree::tree()
 {
-
+  root = NULL;
 }
 tree::~tree()
 {
 
+}
+void tree::addNode(int s)
+{
+  if(root = NULL)
+  {
+    root = new node;
+    root->value = s;
+    root->parent = NULL;
+    root->left = NULL;
+    root->right = NULL;
+    return;
+  }
+  else
+  {
+    node *x = root;
+    node *p;
+    while(x != NULL)
+    {
+      if(x->value > s)
+      {
+        p = x;
+        x = x->left;
+      }
+      else
+      {
+        p = x;
+        x = x->right;
+      }
+    }
+    if(p->value > s)
+    {
+      x = new node;
+      p->left = x;
+      x->parent = p;
+      x->left = NULL;
+      x->right = NULL;
+      x->value = s;
+    }
+    else
+    {
+      x = new node;
+      p->right = x;
+      x->parent = p;
+      x->left = NULL;
+      x->right = NULL;
+      x->value = s;
+    }
+  }
 }
